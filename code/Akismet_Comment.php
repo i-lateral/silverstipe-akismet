b227715de6fbe98e71e96b46ec138f6721803754
Morven Lewis-Everley <morven@i-lateral.com>
2013-01-28 17:13:04 +0000

    Added check that tests if extended item is marked as / not spam when saved, if so, send result to Akismet

--- code/Akismet_Comment.php
+++ code/Akismet_Comment.php
@@ -8,18 +8,38 @@ class Akismet_Comment extends DataExtension {
     public function onBeforeWrite() {					
 		// If Akismet is enabled
 		if(SSAkismet::isEnabled()) {
-			try {
-			    $akismet = new SSAkismet();
-				$akismet->setCommentAuthor($this->owner->Name);
-				$akismet->setCommentContent($this->owner->Comment);
+		    // If new, send to Akismet for cleaning
+		    if(!$this->owner->ID) {
+			    try {
+			        $akismet = new SSAkismet();
+				    $akismet->setCommentAuthor($this->owner->Name);
+				    $akismet->setCommentContent($this->owner->Comment);
 				
-				$result = (int)$akismet->isCommentSpam();
+				    $result = (int)$akismet->isCommentSpam();
 				
-				if($result)
-					if(SSAkismet::getSaveSpam()) $this->owner->IsSpam = true;
+				    if($result)
+					    if(SSAkismet::getSaveSpam()) $this->owner->IsSpam = true;
 					
-			} catch (Exception $e) {
-				// Akismet didn't work, most likely the service is down.
+			    } catch (Exception $e) {
+				    // Akismet didn't work, most likely the service is down.
+			    }
+			}
+			
+			// If spam status has been changed, update Akismet
+			if($this->owner->ID && $this->owner->isChanged('IsSpam')) {
+			    try {
+					$akismet = new SSAkismet();
+					$akismet->setCommentAuthor($this->owner->Name);
+					$akismet->setCommentContent($this->owner->Comment);
+					
+					if($this->owner->IsSpam)
+					    $akismet->submitSpam();
+				    else
+					    $akismet->submitHam();
+				    
+				} catch (Exception $e) {
+					// Akismet didn't work, most likely the service is down.
+				}
 			}
 		}        
     }
