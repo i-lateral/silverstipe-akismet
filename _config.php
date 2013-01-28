99a7c6ff6c09d6b7c901d3bc3be4404c123a9505
Morven Lewis-Everley <morven@i-lateral.com>
2013-01-28 17:16:07 +0000

    BUG: Only extend comment class if it is available in the manifest

--- _config.php
+++ _config.php
@@ -1,4 +1,5 @@
 <?php
 
-// Add Akismet extension
-Object::add_extension('Comment', 'Akismet_Comment');
+// Add Akismet extension to comment, if it exists
+if(class_exists('Comment'))
+    Object::add_extension('Comment', 'Akismet_Comment');
